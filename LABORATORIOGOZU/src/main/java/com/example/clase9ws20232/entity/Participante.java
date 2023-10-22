610be31d53c9937dd53f8ce18fa03f55fc113754
Mortem <a20203607@pucp.edu.pe>
2023-10-22 01:29:41 -0500

    dsa

--- LABORATORIOGOZU/src/main/java/com/example/clase9ws20232/entity/Participante.java
+++ LABORATORIOGOZU/src/main/java/com/example/clase9ws20232/entity/Participante.java
@@ -1,4 +1,32 @@
 package com.example.clase9ws20232.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "participante")
+@Getter
+@Setter
 public class Participante {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idparticipante", nullable = false)
+    private Integer idparticipante;
+
+    @ManyToOne
+    @JoinColumn(name = "equipo")
+    private Equipo equipo;
+
+    @Column(name = "carrera", nullable = false)
+    private String carrera;
+
+    @Column(name = "codigo", nullable = false)
+    private String codigo;
+
+    @Column(name = "tipoParticipante", nullable = false)
+    private String tipoParticipante;
 }
--- LABORATORIOGOZU/src/main/java/com/example/clase9ws20232/entity/Partido.java
+++ LABORATORIOGOZU/src/main/java/com/example/clase9ws20232/entity/Partido.java
@@ -1,4 +1,35 @@
 package com.example.clase9ws20232.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "partido")
+@Getter
+@Setter
 public class Partido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idpartido", nullable = false)
+    private Integer idpartido;
+
+    @ManyToOne
+    @JoinColumn(name = "equipoa")
+    private Equipo equipoa;
+
+    @ManyToOne
+    @JoinColumn(name = "equipob")
+    private Equipo equipob;
+
+    @Column(name = "scorea", nullable = false)
+    private String scorea;
+
+    @Column(name = "scoreb", nullable = false)
+    private String scoreb;
+
+
+
 }
