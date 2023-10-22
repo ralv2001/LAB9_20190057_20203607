e45b413a53c1615d0063b27470ad6c9cdb1e2c74
Ricardo Alvarado <a20190057@pucp.edu.pe>
2023-10-22 12:18:15 -0500

    Doofenshmirtz es el padre de Phineas :o

--- LABORATORIOGOZU/src/main/java/com/example/clase9ws20232/repository/DeporteRepository.java
+++ LABORATORIOGOZU/src/main/java/com/example/clase9ws20232/repository/DeporteRepository.java
@@ -1,4 +1,9 @@
 package com.example.clase9ws20232.repository;
 
-public interface DeporteRepository {
+import com.example.clase9ws20232.entity.Deporte;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DeporteRepository extends JpaRepository<Deporte,Integer> {
 }
