94207a85c42e6e62c99f3604384416b0191915aa
Ricardo Alvarado <a20190057@pucp.edu.pe>
2023-10-22 01:29:07 -0500

    gaaaaaaa

--- LABORATORIOGOZU/src/main/java/com/example/clase9ws20232/entity/HistorialPartidos.java
+++ LABORATORIOGOZU/src/main/java/com/example/clase9ws20232/entity/HistorialPartidos.java
@@ -1,13 +1,30 @@
 package com.example.clase9ws20232.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Date;
 
 @Entity
 @Table(name = "HistorialPartidos")
 @Getter
 @Setter
 public class HistorialPartidos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idhistorialPartidos", nullable = false)
+    private Integer idhistorialPartidos;
+
+    @ManyToOne
+    @JoinColumn(name = "partido_idpartido")
+    private Partido partido;
+
+    @ManyToOne
+    @JoinColumn(name = "deporte_iddeporte")
+    private Deporte deporte;
+
+    @Column(name = "horaFecha")
+    private Date horaFecha;
 }
--- LABORATORIOGOZU/src/main/java/com/example/clase9ws20232/entity/Participante.java
+++ LABORATORIOGOZU/src/main/java/com/example/clase9ws20232/entity/Participante.java
@@ -1,4 +1,31 @@
 package com.example.clase9ws20232.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "participante")
+@Getter
+@Setter
 public class Participante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idparticipante", nullable = false)
+    private Integer idparticipante;
+
+    @ManyToOne
+    @JoinColumn(name = "partido_idpartido")
+    private Partido partido;
+
+    @ManyToOne
+    @JoinColumn(name = "deporte_iddeporte")
+    private Deporte deporte;
+
+    @Column(name = "horaFecha")
+    private Date horaFecha;
+
 }
