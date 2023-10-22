8f31d59324a52961c6ba19d6e20ef81679369f09
Mortem <a20203607@pucp.edu.pe>
2023-10-22 02:00:21 -0500

    dsadsa

--- LABORATORIOGOZU/src/main/java/com/example/clase9ws20232/repository/EquipoRepository.java
+++ LABORATORIOGOZU/src/main/java/com/example/clase9ws20232/repository/EquipoRepository.java
@@ -1,4 +1,11 @@
 package com.example.clase9ws20232.repository;
 
-public interface EquipoRepository {
+
+import com.example.clase9ws20232.entity.Equipo;
+import com.example.clase9ws20232.entity.Partido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EquipoRepository extends JpaRepository<Equipo,Integer> {
 }
