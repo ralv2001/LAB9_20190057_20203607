90d539632b7ba6f1b4df3bb9d2dd3202621cd6e5
Ricardo Alvarado <a20190057@pucp.edu.pe>
2023-10-22 13:43:38 -0500

    Doofenshmirtz es el padre de Phineas :o

--- LABORATORIOGOZU/src/main/java/com/example/clase9ws20232/controller/EquipoController.java
+++ LABORATORIOGOZU/src/main/java/com/example/clase9ws20232/controller/EquipoController.java
@@ -3,6 +3,7 @@ package com.example.clase9ws20232.controller;
 import com.example.clase9ws20232.entity.Equipo;
 import com.example.clase9ws20232.repository.EquipoRepository;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -28,13 +29,29 @@ public class EquipoController {
             @RequestParam(value = "fetchId", required = false) boolean fetchId) {
 
         HashMap<String, Object> responseMap = new HashMap<>();
+        try {
+            equipoRepository.save(equipo);
+            if (fetchId) {
+                responseMap.put("id", equipo.getIdequipo());
+            }
+            responseMap.put("estado", "creado");
+            return ResponseEntity.status(HttpStatus.CREATED).body(responseMap);
+        } catch (DataIntegrityViolationException e) {
+            responseMap.put("estado", "error");
 
-        equipoRepository.save(equipo);
-        if (fetchId) {
-            responseMap.put("id", equipo.getIdequipo());
+            if (e.getMessage().contains("nombre_UNIQUE")) {
+                responseMap.put("msg", "El nombre del equipo ya existe en la base de datos.");
+            } else if (e.getMessage().contains("color_UNIQUE")) {
+                responseMap.put("msg", "El color del equipo ya existe en la base de datos.");
+            } else if (e.getMessage().contains("mascota_UNIQUE")) {
+                responseMap.put("msg", "La mascota del equipo ya existe en la base de datos.");
+            } else {
+                responseMap.put("msg", "Error de integridad en la base de datos.");
+            }
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseMap);
         }
-        responseMap.put("estado", "creado");
-        return ResponseEntity.status(HttpStatus.CREATED).body(responseMap);
+
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
@@ -47,5 +64,4 @@ public class EquipoController {
         }
         return ResponseEntity.badRequest().body(responseMap);
     }
-
 }
